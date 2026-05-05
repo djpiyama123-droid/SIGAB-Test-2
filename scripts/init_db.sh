12db81f264af5c742b3789381d69e7ba3c435628
djpiyama123-droid <djpiyama123@gmail.com>
2026-05-05 11:34:35 -0700

    chore: sync deleted/moved files from remote main

--- scripts/init_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo service mysql start
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS sigab;"
-sudo mysql -e "CREATE USER IF NOT EXISTS 'sigab_user'@'localhost' IDENTIFIED BY 'sigab_pass_2026';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON sigab.* TO 'sigab_user'@'localhost';"
-sudo mysql -e "FLUSH PRIVILEGES;"
-mysql -u sigab_user -psigab_pass_2026 sigab < sigab_schema_fresh.sql
-mysql -u sigab_user -psigab_pass_2026 sigab < seed_data.sql
-echo "Base de datos inicializada y poblada exitosamente."
